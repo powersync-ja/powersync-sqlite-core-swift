de3436767d5ec1f7b6cf4e0ef1b04c84836a4d1b
DominicGBauer <dominic@nomanini.com>
2024-10-14 11:21:00 +0200

    feat: add spm release flow

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+let packageName = "PowerSyncSQLiteCore"
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: "SHOULD BE UPDATED BY GITHUB ACTION",
+            checksum: "SHOULD BE UPDATED BY GITHUB ACTION"
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# powersync-sqlite-core-swift
-Swift Package Manager version of powersync-sqlite-core
+# PowerSync SQLite Core Swift Package
+
+This is the Swift Package Manager version of[powersync-sqlite-core](https://github.com/powersync-ja/powersync-sqlite-core). See CHANGELOG and more information in that repo.
